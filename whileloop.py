548c2b4911e792fb86b9e24a7270ece602115d8a
samarth0157 <samarth3112@live.com>
2019-11-17 18:26:00 +0530

    updating programs

---
+++ whileloop.py
@@ -0,0 +1,14 @@
+import math
+
+i = 1
+
+while i <= 5:
+
+    print("Samarth", end="")
+    j = 1
+    while j <= 4:
+        print("devops", end="")
+        j = j + 1
+
+    i = i + 1
+    print()
